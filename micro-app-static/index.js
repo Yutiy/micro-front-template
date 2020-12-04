071434ff2da36d7cc00e5c606a9df9a59de5a385
杨林 <494657028@qq.com>
2020-12-04 15:48:30 +0800

    初始化各个微应用

---
+++ micro-app-static/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// 解决跨域问题
+app.use(cors());
+app.use('/', express.static('static'));
+
+// 监听端口
+app.listen(7001, () => {
+  console.log("server is listening in http://localhost:7001")
+})
